c97caa7bbcca6b63160bc5a0eb658dfa666dc3c5
Daniel Yang <3014022+daniel-yj-yang@users.noreply.github.com>
2021-11-24 20:13:05 -0600

    Update

---
+++ treekit/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Author: Daniel Yang <daniel.yj.yang@gmail.com>
+#
+# License: MIT
+
+
+from .__about__ import (
+    __version__,
+    __license__,
+)
+
+from .binarytree import binarytree
+
+
+# this is for "from <package_name> import *"
+__all__ = ["binarytree", ]
