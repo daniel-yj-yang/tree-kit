4f4dcf5fa8f7b22ecd91ac704e87b102c7d61989
Daniel Yang <3014022+daniel-yj-yang@users.noreply.github.com>
2021-11-24 20:14:59 -0600

    Update

---
+++ treekit/binarytree/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Author: Daniel Yang <daniel.yj.yang@gmail.com>
+#
+# License: MIT
+
+from ._binarytree import binarytree
+
+# this is for "from <package_name>.game import *"
+__all__ = ["binarytree",]
---
+++ treekit/binarytree/_binarytree.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Author: Daniel Yang <daniel.yj.yang@gmail.com>
+#
+# License: MIT
+
+class binarytree()
+    def __init__(self):
+        pass
