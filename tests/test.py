a8a4c2046ca08be8dc1669e02f44f9fbccf5876d
Daniel Yang <3014022+daniel-yj-yang@users.noreply.github.com>
2021-11-26 20:19:51 -0600

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -6,8 +6,8 @@
 
 from treekit import binarytree, bst
 bt1 = binarytree([15, 7, 23, 3, 11, 19, 27, 1, 5, 9, 13, 17, 21, 25, 29, 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30])
-bt1.inorder
-bt1.preorder
-bt1.postorder
-bt1.levelorder
+print(bt1.inorder)
+print(bt1.preorder)
+print(bt1.postorder)
+print(bt1.levelorder)
 bst1 = bst(h=6)
